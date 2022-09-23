e14728947609559b97650f991f88145ac42c7ddc
Ifan Johnston <ifan.johnston@gmail.com>
2022-09-23 19:08:55 +0200

    Add config.ts and deploy to vercel

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'What I use for development - Otherwise'
+export const SITE_DESCRIPTION =
+  'A blog which lists all the things I use for development, both professionally and during hobby coding.'
+export const HOMEPAGE_URL = 'https://otherwise-uses.vercel.app'
