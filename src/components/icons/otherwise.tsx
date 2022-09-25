d41e1d0ad94c508f28a4aef066e3bc53b925ba71
Ifanj <ifan.johnston@gmail.com>
2022-09-25 14:03:35 +0200

    Make otherwise logo smaller and narrower, add content component

--- src/components/icons/otherwise.tsx
+++ src/components/icons/otherwise.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export default function OtherwiseIcon() {
   return (
     <svg
-      className="w-20 h-20 inline-block transition-transform group-hover:rotate-[-20deg]"
+      className="w-[1.5rem] h-[1.5rem] inline-block transition-transform group-hover:rotate-[-20deg]"
       xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 25 25"
     >
       <g
         className="overflow-hidden"
@@ -16,12 +17,12 @@ export default function OtherwiseIcon() {
         }}
       >
         <text className="fill-red-600" y="50%" x="50%">
-          therwise
+          w
         </text>
         <circle
           className="stroke-red-500 stroke-1 fill-white group-hover:fill-gray-400"
           cx="50%"
-          cy="45%"
+          cy="40%"
           r="30%"
           fill-opacity="0.3"
         />
