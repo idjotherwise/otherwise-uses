293158cd52c15efc599a5dd015e97217c6e2d733
Ifanj <ifan.johnston@gmail.com>
2022-10-23 23:03:11 +0200

    Added tailwind typograph to posts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
-	theme: {
-		extend: {
+  theme: {
+    extend: {
       fontFamily: {
         mplus: ["'M PLUS Rounded 1c'", 'Verdana', 'sans-serif']
       }
-    },
-	},
-	plugins: [],
+    }
+  },
+  plugins: [require('@tailwindcss/typography')]
 }
